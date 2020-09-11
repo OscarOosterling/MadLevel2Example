356e492132bdb5ce6f3b6cc68b6d77f3e36f0ab8
OscarOosterling <oscar-oosterling@live.nl>
2020-09-11 13:38:54 +0200

    completed example

--- app/src/main/java/com/example/madlevel2example/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2example/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.madlevel2example
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2example.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_reminder.view.*
 
 class MainActivity : AppCompatActivity() {
@@ -33,6 +36,13 @@ class MainActivity : AppCompatActivity() {
         binding.rvReminders.addItemDecoration(DividerItemDecoration(this@MainActivity,DividerItemDecoration.VERTICAL))
     }
     private fun addReminder(reminder:String){
-        
+        if(reminder.isNotBlank()){
+            Log.d(reminders.toString(),"test")
+            reminders.add(Reminder(reminder))
+            reminderAdapter.notifyDataSetChanged()
+            binding.etReminder.text?.clear()
+        }else{
+            Snackbar.make(etReminder,"You must fill in the inputfield!",Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
